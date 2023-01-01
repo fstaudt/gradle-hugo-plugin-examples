e634117097543460aedf7d2b9672370ec7fae56b
fstaudt <fstaudt@gmail.com>
2023-01-01 18:19:16 +0100

    feat: bump spring boot to 3.0.0

--- spring-boot/build.gradle.kts
+++ spring-boot/build.gradle.kts
@@ -1,55 +1,53 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.JavaVersion.VERSION_17
 
 plugins {
-	id("org.springframework.boot") version "2.7.4"
-	id("io.spring.dependency-management") version "1.0.14.RELEASE"
-	id("io.github.fstaudt.hugo") version "0.5.0"
-	war
-	kotlin("jvm") version "1.7.20"
-	kotlin("plugin.spring") version "1.7.20"
+    id("org.springframework.boot") version "3.0.0"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("io.github.fstaudt.hugo") version "0.5.0"
+    war
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.spring") version "1.7.20"
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+tasks.compileKotlin {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+tasks.test { useJUnitPlatform() }
 
 hugo {
-	sourceDirectory = "src/main/hugo"
+    sourceDirectory = "src/main/hugo"
 }
 tasks.hugoServer {
-	baseURL = "http://localhost:1313/documentation"
+    baseURL = "http://localhost:1313/documentation"
 }
 tasks.hugoBuild {
-	publicationPath = "static/documentation"
-	sourceSets {
-		main {
-			resources {
-				srcDir(outputDirectory)
-			}
-		}
-	}
+    publicationPath = "static/documentation"
+    sourceSets {
+        main {
+            resources {
+                srcDir(outputDirectory)
+            }
+        }
+    }
 }
 tasks.processResources { dependsOn(tasks.hugoBuild) }
 tasks.classes { dependsOn(tasks.hugoBuild) }
